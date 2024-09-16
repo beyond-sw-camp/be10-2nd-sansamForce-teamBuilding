271e3b2a3f30eb5e72a00c80ef9c5d8fa771f9d3
kookong2 <hy010309@naver.com>
2024-09-17 03:24:20 +0900

    [CHORE] 파일 디렉토리 변경으로 인한 임포트 변경

--- team/src/main/java/sansam/team/user/command/service/UserService.java
+++ team/src/main/java/sansam/team/user/command/service/UserService.java
@@ -4,21 +4,17 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import sansam.team.common.JWTUtil;
+import sansam.team.common.jwt.JWTUtil;
 import sansam.team.user.command.dto.JwtToken;
 import sansam.team.user.command.dto.LoginRequestDTO;
 import sansam.team.user.command.dto.UserDTO;
 import sansam.team.user.command.dto.UserJoinDTO;
 import sansam.team.user.command.entity.User;
 import sansam.team.user.command.repository.UserRepository;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
