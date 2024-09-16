a2106b89b3972a40044d9b16e112ce65b0afd982
kookong2 <hy010309@naver.com>
2024-09-17 03:24:37 +0900

    [FEAT] ProjectBoardController 코드 추가

---
+++ team/src/main/java/sansam/team/project/command/controller/ProjectBoardController.java
@@ -0,0 +1,37 @@
+package sansam.team.project.command.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sansam.team.project.command.dto.ProjectBoardDTO;
+import sansam.team.project.command.entity.ProjectBoard;
+import sansam.team.project.command.service.ProjectBoardService;
+
+@RestController
+@RequestMapping("api/v1/admin/project/board")
+@RequiredArgsConstructor
+public class ProjectBoardController {
+
+    private final ProjectBoardService projectBoardService;
+
+    @PostMapping
+    public ResponseEntity<ProjectBoard> createProjectBoard(@RequestBody ProjectBoardDTO projectBoardDTO) {
+        ProjectBoard projectBoard = projectBoardService.createProjectBoard(projectBoardDTO);
+        return ResponseEntity.ok(projectBoard);
+    }
+
+    @PutMapping("/{projectBoardSeq}")
+    public ResponseEntity<ProjectBoard> updateProjectBoard(
+            @PathVariable Long projectBoardSeq,
+            @RequestBody ProjectBoardDTO projectBoardDTO) {
+        ProjectBoard updatedProjectBoard = projectBoardService.updateProjectBoard(projectBoardSeq, projectBoardDTO);
+        return ResponseEntity.ok(updatedProjectBoard);
+    }
+
+    @DeleteMapping("/{projectBoardSeq}")
+    public ResponseEntity<Void> deleteProjectBoard(@PathVariable Long projectBoardSeq) {
+        projectBoardService.deleteProjectBoard(projectBoardSeq);
+        return ResponseEntity.noContent().build();
+    }
+
+}
