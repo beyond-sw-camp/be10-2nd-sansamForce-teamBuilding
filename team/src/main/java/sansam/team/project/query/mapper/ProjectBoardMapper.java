8b085f8ab65d78c97999327a7b666781f54c3358
kookong2 <hy010309@naver.com>
2024-09-19 11:37:54 +0900

    [FEAT] 프로젝트 신청 회원 리스트 조회 기능 추가

--- team/src/main/java/sansam/team/project/query/mapper/ProjectBoardMapper.java
+++ team/src/main/java/sansam/team/project/query/mapper/ProjectBoardMapper.java
@@ -1,13 +1,19 @@
 package sansam.team.project.query.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
-import sansam.team.project.query.dto.ProjectBoardQueryDTO;
+import org.apache.ibatis.annotations.Param;
+import sansam.team.project.query.dto.projectboard.ProjectApplyMemberQueryDTO;
+import sansam.team.project.query.dto.projectboard.ProjectBoardQueryDTO;
 
 import java.util.List;
 
 @Mapper
 public interface ProjectBoardMapper {
+
     List<ProjectBoardQueryDTO> findAll();
 
     ProjectBoardQueryDTO findById(Long projectBoardSeq);
+
+    // 특정 프로젝트에 신청한 회원들을 조회하는 메서드
+    List<ProjectApplyMemberQueryDTO> findApplyMembersByProjectBoardSeq(@Param("projectBoardSeq") Long projectBoardSeq);
 }
