27efb64b476098524c513912c5644cfe02f6bc3a
kookong2 <hy010309@naver.com>
2024-09-20 15:57:47 +0900

    [REFACTOR] 프로젝트 게시글 수정

--- team/src/main/java/sansam/team/project/command/entity/ProjectBoard.java
+++ team/src/main/java/sansam/team/project/command/entity/ProjectBoard.java
@@ -2,12 +2,11 @@ package sansam.team.project.command.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import sansam.team.common.embedded.Auditable;
-import sansam.team.common.embedded.AuditableEntity;
-import sansam.team.common.embedded.BaseEntity;
+import org.springframework.data.domain.Auditable;
+import sansam.team.common.BaseTimeEntity;
 import sansam.team.project.command.enums.BoardStatus;
 import sansam.team.user.command.entity.User;
 
@@ -19,64 +18,34 @@ import java.util.List;
 @Table(name = "tbl_project_board")
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
-@EntityListeners(BaseEntity.class)
-public class ProjectBoard implements AuditableEntity {
+public class ProjectBoard extends BaseTimeEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "project_board_seq")
     private Long projectBoardSeq;
 
-    @Column(name = "project_board_title", nullable = false)
     private String projectBoardTitle;
 
-    @Column(name = "project_board_content", nullable = false)
     private String projectBoardContent;
 
-    @Column(name = "project_board_head_count", nullable = false)
     private int projectBoardHeadCount;
 
-    @Column(name = "project_board_img_url")
     private String projectBoardImgUrl;
 
-    @Column(name = "project_board_start_date", nullable = false)
     private LocalDateTime projectBoardStartDate;
 
-    @Column(name = "project_board_end_date")
     private LocalDateTime projectBoardEndDate;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "project_board_status", nullable = false)
     private BoardStatus boardStatus = BoardStatus.RECRUITMENT;
 
-    // Auditable 필드를 초기화
-    @Embedded
-    private Auditable auditable = new Auditable(); // 여기에서 기본값으로 초기화
-
-    @Column(name = "project_start_date", nullable = false)
     private LocalDateTime projectStartDate;
 
-    @Column(name = "project_end_date")
     private LocalDateTime projectEndDate;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_seq", nullable = false)
-    @JsonIgnore // 순환 참조 방지: 직렬화에서 무시
-    private User user;
+    private Long projectBoardAdminSeq;
 
-    @OneToMany(mappedBy = "projectBoard", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<ProjectApplyMember> projectApplyMembers = new ArrayList<>();
-
-    @Override
-    public Auditable getAuditable() {
-        return auditable;
+    @Builder
+    public ProjectBoard(String projectBoardTitle, String projectBoardContent, int projectBoardHeadCount, String projectBoardImgUrl, LocalDateTime projectBoardStartDate, LocalDateTime projectBoardEndDate, BoardStatus boardStatus, LocalDateTime projectStartDate, LocalDateTime projectEndDate, User user) {
+        super();
     }
-
-    @Override
-    public void setAuditable(Auditable auditable) {
-        this.auditable = auditable;
-    }
-
-
 }
