d961f6039dc61c874b9d92763364efd90500e5a3
kookong2 <hy010309@naver.com>
2024-09-20 15:57:34 +0900

    [REFACTOR] 프로젝트 신청 회원 엔티티 수정

--- team/src/main/java/sansam/team/project/command/entity/ProjectApplyMember.java
+++ team/src/main/java/sansam/team/project/command/entity/ProjectApplyMember.java
@@ -1,53 +1,26 @@
 package sansam.team.project.command.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import sansam.team.common.embedded.Auditable;
-import sansam.team.common.embedded.AuditableEntity;
-import sansam.team.common.embedded.BaseEntity;
+import sansam.team.common.BaseTimeEntity;
+
 import sansam.team.project.command.enums.ApplyStatus;
-import sansam.team.user.command.entity.User;
 
 @Entity
 @Table(name = "tbl_project_apply_member")
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
-@EntityListeners(BaseEntity.class)
-public class ProjectApplyMember implements AuditableEntity {
+public class ProjectApplyMember extends BaseTimeEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "project_apply_member_seq")
-    private Long projectApplyMemberSeq;
+    private Long projectApplyMemberSeq;                 // 프로젝트 신청 테이블 번호
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "project_apply_member_status", nullable = false)
-    private ApplyStatus projectApplyMemberStatus = ApplyStatus.APPLIED; // 지원 상태
+    private ApplyStatus projectApplyMemberStatus = ApplyStatus.APPLIED;     // 프로젝트 신청 회원 상태 (신청, 허가, 거부)
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_seq", nullable = false)
-    @JsonIgnore
-    private User user;  // User와 1:N 관계
+    private Long userSeq;                               // 프로젝트 신청 회원 번호
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "project_board_seq", nullable = false)
-    @JsonIgnore
-    private ProjectBoard projectBoard;  // ProjectBoard와 1:N 관계
+    private Long projectBoardSeq;                       // 프로젝트 게시글 번호
 
-    @Embedded
-    private Auditable auditable = new Auditable();
-
-    @Override
-    public Auditable getAuditable() {
-        return auditable;
-    }
-
-    @Override
-    public void setAuditable(Auditable auditable) {
-        this.auditable = auditable;
-    }
 }
