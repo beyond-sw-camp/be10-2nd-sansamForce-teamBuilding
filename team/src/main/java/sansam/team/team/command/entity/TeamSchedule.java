823017a72e5a934ab9b29c71ffb35bf8ceb0be4a
beanteacher <dh65432@naver.com>
2024-09-20 17:15:32 +0900

    [FEAT] 팀 일정, 팀원 일정 진행상황 엔티티 추가

---
+++ team/src/main/java/sansam/team/team/command/entity/TeamSchedule.java
@@ -0,0 +1,30 @@
+package sansam.team.team.command.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sansam.team.common.BaseTimeEntity;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@SequenceGenerator(
+        name = "TEAM_SCHEDULE_SEQ_GENERATOR"
+        , sequenceName = "TEAM_SCHEDULE_SEQ"
+        , allocationSize = 1)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TeamSchedule extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO, generator = "TEAM_SCHEDULE_SEQ_GENERATOR")
+    private long teamScheduleSeq;
+    private long teamSeq;
+    @Column(name = "team_schedule_content")
+    private long content;
+    @Column(name = "team_schedule_start_date")
+    private LocalDateTime startDate;
+    @Column(name = "team_schedule_end_date")
+    private LocalDateTime endDate;
+}
