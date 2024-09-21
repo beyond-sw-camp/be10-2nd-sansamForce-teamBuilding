2979dbcf5f279775cab0937ee3e9695ddb777afd
kookong2 <hy010309@naver.com>
2024-09-22 00:19:56 +0900

    [REFACTOR] 회원가입 DTO 변경
    회원 가입에 필요한 사항만 남겨둠

--- team/src/main/java/sansam/team/user/command/dto/UserJoinDTO.java
+++ team/src/main/java/sansam/team/user/command/dto/UserJoinDTO.java
@@ -13,31 +13,18 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserJoinDTO {
-    private Long userSeq;
+
     private String id;
     private String name;
     private String nickname;
     private String password;
-    private RoleType auth;
     private String phone;
     private String email;
     private String birthDate;
     private String gender;
-    private String githubId;
-    private String profileImg;
-    private StatusType status;
-    private LocalDateTime pwdModDate;
-    private LocalDateTime regDate;
-    private LocalDateTime modDate;
-    private LocalDateTime banDate;
-    private LocalDateTime delDate;
 
     public void changePasswordEncoder(String password) {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         this.password = bCryptPasswordEncoder.encode(password);
-    }
-
-    public void changeAuthMember() {
-        this.auth = RoleType.MEMBER;
     }
 }
