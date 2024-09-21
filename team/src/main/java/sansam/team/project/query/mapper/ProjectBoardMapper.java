bcbbdc9cd6dd5aa1af88d9264155de0fc3eca679
kookong2 <hy010309@naver.com>
2024-09-22 03:52:19 +0900

    [FEAT] 프로젝트 게시물 유저 전체조회, 상세 조회 기능 추가

--- team/src/main/java/sansam/team/project/query/mapper/ProjectBoardMapper.java
+++ team/src/main/java/sansam/team/project/query/mapper/ProjectBoardMapper.java
@@ -4,7 +4,8 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import sansam.team.project.query.dto.projectboard.ProjectApplyMemberQueryDTO;
 import sansam.team.project.query.dto.projectboard.ProjectBoardAllQueryDTO;
-import sansam.team.project.query.dto.projectboard.ProjectBoardQueryDTO;
+import sansam.team.project.query.dto.projectboard.ProjectBoardAdminDTO;
+import sansam.team.project.query.dto.projectboard.ProjectBoardUserDTO;
 
 import java.util.List;
 
@@ -13,7 +14,9 @@ public interface ProjectBoardMapper {
 
     List<ProjectBoardAllQueryDTO> findAll();
 
-    ProjectBoardQueryDTO findById(Long projectBoardSeq);
+    ProjectBoardAdminDTO findByIdForAdmin(Long projectBoardSeq);
+
+    ProjectBoardUserDTO findByIdForUser(Long projectBoardSeq);
 
     // 특정 프로젝트에 신청한 회원들을 조회하는 메서드
     List<ProjectApplyMemberQueryDTO> findApplyMembersByProjectBoardSeq(@Param("projectBoardSeq") Long projectBoardSeq);
--- team/src/main/java/sansam/team/project/query/service/ProjectBoardQueryService.java
+++ team/src/main/java/sansam/team/project/query/service/ProjectBoardQueryService.java
@@ -4,8 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import sansam.team.project.query.dto.projectboard.ProjectApplyMemberQueryDTO;
 import sansam.team.project.query.dto.projectboard.ProjectBoardAllQueryDTO;
-import sansam.team.project.query.dto.projectboard.ProjectBoardQueryDTO;
-import sansam.team.project.query.mapper.ProjectApplyMemberMapper;
+import sansam.team.project.query.dto.projectboard.ProjectBoardAdminDTO;
+import sansam.team.project.query.dto.projectboard.ProjectBoardUserDTO;
 import sansam.team.project.query.mapper.ProjectBoardMapper;
 
 import java.util.List;
@@ -21,9 +21,14 @@ public class ProjectBoardQueryService {
         return projectBoardMapper.findAll();
     }
 
-    /* 프로젝트 게시물 상세 조회 */
-    public ProjectBoardQueryDTO getProjectBoardById(Long projectBoardSeq) {
-        return projectBoardMapper.findById(projectBoardSeq);
+    /* 프로젝트 게시물 상세 조회 (관리자) */
+    public ProjectBoardAdminDTO getProjectBoardByIdForAdmin(Long projectBoardSeq) {
+        return projectBoardMapper.findByIdForAdmin(projectBoardSeq);
+    }
+
+    /* 프로젝트 게시물 상세 조회 (사용자) */
+    public ProjectBoardUserDTO getProjectBoardByIdForUser(Long projectBoardSeq){
+        return projectBoardMapper.findByIdForUser(projectBoardSeq);
     }
 
     /* 프로젝트 신청 회원 리스트 조회 */
