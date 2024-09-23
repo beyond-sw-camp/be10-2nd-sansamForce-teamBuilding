5f0cecfdb3c1190713bf12d3cf9ea25452f0997d
kookong2 <hy010309@naver.com>
2024-09-23 19:22:22 +0900

    [FEAT] ProjectRepository 수정, 삭제 추가

--- team/src/main/java/sansam/team/project/command/domain/repository/ProjectRepository.java
+++ team/src/main/java/sansam/team/project/command/domain/repository/ProjectRepository.java
@@ -2,7 +2,13 @@ package sansam.team.project.command.domain.repository;
 
 import sansam.team.project.command.domain.aggregate.entity.Project;
 
+import java.util.Optional;
+
 public interface ProjectRepository {
 
     Project save(Project project);
+
+    Optional<Project> findById(Long projectSeq);
+
+    void deleteById(Long projectSeq);
 }
