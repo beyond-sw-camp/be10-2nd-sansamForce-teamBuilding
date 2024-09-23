db05f24faf043b093518d5b93c2acb1f1ee4a9df
kookong2 <hy010309@naver.com>
2024-09-23 17:25:07 +0900

    [FEAT] SecurityUtil 클래스 추가
    해당 메서드 호출함으로써 user 객체에서 Seq 값 추출 가능

---
+++ team/src/main/java/sansam/team/common/jwt/SecurityUtil.java
@@ -0,0 +1,16 @@
+package sansam.team.common.jwt;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import sansam.team.user.command.domain.aggregate.entity.User;
+
+public class SecurityUtil {
+
+    public static User getAuthenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof User) {
+            return (User) authentication.getPrincipal();
+        }
+        throw new IllegalArgumentException("User not authenticated");
+    }
+}
