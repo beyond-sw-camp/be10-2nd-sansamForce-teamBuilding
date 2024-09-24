124f999d0885d648f6c87a2b5bd38ef4bd1087b5
kookong2 <hy010309@naver.com>
2024-09-24 20:20:59 +0900

    [REFACTOR] ProjectMemberMapper 수정

--- team/src/main/java/sansam/team/project/command/mapper/ProjectMemberMapper.java
+++ team/src/main/java/sansam/team/project/command/mapper/ProjectMemberMapper.java
@@ -1,15 +1,12 @@
 package sansam.team.project.command.mapper;
 
-import sansam.team.project.command.application.dto.project.ProjectMemberCreateDTO;
 import sansam.team.project.command.domain.aggregate.entity.ProjectMember;
 
 public class ProjectMemberMapper {
 
-    public static ProjectMember toEntity(Long userSeq, Long projectSeq, ProjectMemberCreateDTO projectMemberCreateDTO) {
+    public static ProjectMember toEntity(Long userSeq, Long projectSeq) {
 
         return ProjectMember.createEntity(
-                projectMemberCreateDTO.getProjectMemberDelYn(),
-                projectMemberCreateDTO.getProjectMentorYn(),
                 userSeq,
                 projectSeq
         );
