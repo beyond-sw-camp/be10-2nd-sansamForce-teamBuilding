e224032a08715635dde44e8d16eae4d7d9c3094f
kookong2 <hy010309@naver.com>
2024-09-25 00:50:15 +0900

    [FEAT] ProjectAllQueryDTO 추가

--- team/src/main/java/sansam/team/project/query/dto/project/ProjectAllQueryDTO.java
+++ team/src/main/java/sansam/team/project/query/dto/project/ProjectAllQueryDTO.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import sansam.team.project.command.domain.aggregate.ProjectStatus;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -14,7 +16,7 @@ public class ProjectAllQueryDTO {
 
     private Long projectSeq;
     private String projectTitle;
-    private String projectContent;
     private ProjectStatus projectStatus;
+    private String projectImgUrl;
 
 }
