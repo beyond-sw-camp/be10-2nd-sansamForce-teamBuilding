13a289cd917806fb3387baea8855c6a003a5486c
kookong2 <hy010309@naver.com>
2024-09-25 00:50:00 +0900

    [FEAT] ProjectAdminQueryDTO 추가

---
+++ team/src/main/java/sansam/team/project/query/dto/project/ProjectAdminQueryDTO.java
@@ -0,0 +1,26 @@
+package sansam.team.project.query.dto.project;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import sansam.team.project.command.domain.aggregate.ProjectStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProjectAdminQueryDTO {
+
+    private Long projectSeq;
+    private String projectTitle;
+    private String projectContent;
+    private ProjectStatus projectStatus;
+    private int projectHeadCount;
+    private String projectImgUrl;
+    private LocalDateTime projectStartDate;
+    private LocalDateTime projectEndDate;
+    private Long projectAdminSeq;
+}
