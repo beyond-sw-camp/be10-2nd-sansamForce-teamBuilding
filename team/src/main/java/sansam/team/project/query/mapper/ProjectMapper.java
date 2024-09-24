94fc22be277f3093d22d0f24cfacfbc293891f6e
kookong2 <hy010309@naver.com>
2024-09-25 00:50:46 +0900

    [FEAT] ProjectMapper 인터페이스 추가

--- team/src/main/java/sansam/team/project/query/mapper/ProjectMapper.java
+++ team/src/main/java/sansam/team/project/query/mapper/ProjectMapper.java
@@ -1,4 +1,20 @@
 package sansam.team.project.query.mapper;
 
+import sansam.team.project.query.dto.project.ProjectAdminQueryDTO;
+import sansam.team.project.query.dto.project.ProjectAllQueryDTO;
+import sansam.team.project.query.dto.project.ProjectUserQueryDTO;
+
+import java.util.List;
+
 public interface ProjectMapper {
+
+    List<ProjectAllQueryDTO> findAllProjectForAdmin();
+
+    List<ProjectAllQueryDTO> findAllProjectForUser();
+
+    ProjectAdminQueryDTO findProjectByIdForAdmin(Long projectSeq);
+
+    ProjectUserQueryDTO findProjectByIdForUser(Long projectSeq);
+
+
 }
