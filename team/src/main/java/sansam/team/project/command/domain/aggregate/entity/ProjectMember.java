a6a6875a23a3310bc700b5a5a952b54f51e2a1fa
kookong2 <hy010309@naver.com>
2024-09-24 14:31:29 +0900

    [FEAT] 프로젝트 맴버 Seq 속성 추가

--- team/src/main/java/sansam/team/project/command/domain/aggregate/entity/ProjectMember.java
+++ team/src/main/java/sansam/team/project/command/domain/aggregate/entity/ProjectMember.java
@@ -1,8 +1,6 @@
 package sansam.team.project.command.domain.aggregate.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import sansam.team.common.YnType;
@@ -14,6 +12,7 @@ import sansam.team.common.YnType;
 public class ProjectMember {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int projectMemberSeq;
 
     private YnType projectMemberDelYn = YnType.Y;
