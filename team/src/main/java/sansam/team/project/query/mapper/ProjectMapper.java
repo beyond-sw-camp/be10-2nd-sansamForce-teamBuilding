7b2518c03915e02f17d60db95b68166d5ff51621
kookong2 <hy010309@naver.com>
2024-09-25 09:37:04 +0900

    [REFACTOR] findAllProjectForUser 매개변수 변경

--- team/src/main/java/sansam/team/project/query/mapper/ProjectMapper.java
+++ team/src/main/java/sansam/team/project/query/mapper/ProjectMapper.java
@@ -1,16 +1,18 @@
 package sansam.team.project.query.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
 import sansam.team.project.query.dto.project.ProjectAdminQueryDTO;
 import sansam.team.project.query.dto.project.ProjectAllQueryDTO;
 import sansam.team.project.query.dto.project.ProjectUserQueryDTO;
 
 import java.util.List;
 
+@Mapper
 public interface ProjectMapper {
 
     List<ProjectAllQueryDTO> findAllProjectForAdmin();
 
-    List<ProjectAllQueryDTO> findAllProjectForUser();
+    List<ProjectAllQueryDTO> findAllProjectForUser(Long userSeq);
 
     ProjectAdminQueryDTO findProjectByIdForAdmin(Long projectSeq);
 
