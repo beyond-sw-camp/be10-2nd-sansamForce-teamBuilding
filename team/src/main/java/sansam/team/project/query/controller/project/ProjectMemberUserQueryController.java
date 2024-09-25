aa7374f15a187240515ae97136cbc1de3fcf20ea
kookong2 <hy010309@naver.com>
2024-09-26 00:56:46 +0900

    [FEAT] ProjectMemberUserQueryController 추가

---
+++ team/src/main/java/sansam/team/project/query/controller/project/ProjectMemberUserQueryController.java
@@ -0,0 +1,14 @@
+package sansam.team.project.query.controller.project;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/project/member")
+@RequiredArgsConstructor
+@Tag(name = "Project Member Admin API", description = "프로젝트 회원 관리자 API")
+public class ProjectMemberUserQueryController {
+    
+}
