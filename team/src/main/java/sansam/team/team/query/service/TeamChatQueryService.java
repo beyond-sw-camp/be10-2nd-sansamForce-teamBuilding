a09ee49b9a34e8ef10a3b3ee1a79c84fd5deffbe
beanteacher <dh65432@naver.com>
2024-09-29 14:18:27 +0900

    [FEAT] 채팅방 입장 시 데이터 가져오기 추가

--- team/src/main/java/sansam/team/team/query/service/TeamChatQueryService.java
+++ team/src/main/java/sansam/team/team/query/service/TeamChatQueryService.java
@@ -2,10 +2,18 @@ package sansam.team.team.query.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 import sansam.team.common.jwt.SecurityUtil;
-import sansam.team.team.query.dto.TeamChatDTO;
-import sansam.team.team.query.dto.TeamChatQueryDTO;
+import sansam.team.common.websocket.WebSocketClient;
+import sansam.team.common.websocket.dto.TeamChatMemberDTO;
+import sansam.team.common.websocket.dto.TeamChatMessageDTO;
+import sansam.team.team.query.dto.chat.TeamChatResponse;
+import sansam.team.team.query.dto.chat.TeamChatRequest;
+import sansam.team.team.query.dto.chat.TeamChatRoomRequest;
+import sansam.team.team.query.dto.chat.TeamChatRoomResponse;
 import sansam.team.team.query.mapper.TeamChatQueryMapper;
 import sansam.team.user.command.domain.aggregate.entity.User;
 
@@ -16,13 +24,29 @@ import java.util.List;
 @Slf4j
 public class TeamChatQueryService {
     private final TeamChatQueryMapper teamChatQueryMapper;
+    private final MongoTemplate mongoTemplate;
+    private final WebSocketClient webSocketClient;
 
-    public List<TeamChatDTO> selectChatRoomList() {
+    public List<TeamChatResponse> selectChatRoomList() {
         User user = SecurityUtil.getAuthenticatedUser();
-        return teamChatQueryMapper.selectChatRoomList(new TeamChatQueryDTO(user.getUserSeq(), user.getUserAuth()));
+        return teamChatQueryMapper.selectChatRoomList(new TeamChatRequest(user.getUserSeq(), user.getUserAuth()));
     }
 
-    public TeamChatDTO selectChatRoom(Long teamChatSeq) {
-        return teamChatQueryMapper.selectChatRoom(teamChatSeq);
+    public TeamChatRoomResponse selectChatRoom(Long teamChatSeq) {
+        User user = SecurityUtil.getAuthenticatedUser();
+        TeamChatRoomResponse response = teamChatQueryMapper.selectChatRoom(new TeamChatRoomRequest(teamChatSeq, user.getUserSeq()));
+
+        Query query = new Query();
+        query.addCriteria(Criteria.where("teamChatSeq").is(teamChatSeq));
+        List<TeamChatMessageDTO> teamChatMessageList = mongoTemplate.find(query, TeamChatMessageDTO.class);
+        List<TeamChatMemberDTO> teamChatMemberList = mongoTemplate.find(query, TeamChatMemberDTO.class);
+
+        response.setTeamChatMessageList(teamChatMessageList);
+        response.setTeamChatMemberList(teamChatMemberList);
+
+        System.out.println("response : " + response);
+        webSocketClient.start(response);
+
+        return response;
     }
 }
