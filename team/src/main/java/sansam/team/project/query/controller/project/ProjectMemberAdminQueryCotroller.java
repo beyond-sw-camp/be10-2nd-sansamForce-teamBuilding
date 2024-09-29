31e7c1b82f866a15e7fc0a4a17aa55912c4b908d
kookong2 <hy010309@naver.com>
2024-09-29 19:55:16 +0900

    [FEAT] 관리자 프로젝트 회원 리스트 조회 컨트롤러 구현

--- team/src/main/java/sansam/team/project/query/controller/project/ProjectMemberAdminQueryCotroller.java
+++ team/src/main/java/sansam/team/project/query/controller/project/ProjectMemberAdminQueryCotroller.java
@@ -1,9 +1,16 @@
 package sansam.team.project.query.controller.project;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sansam.team.project.query.dto.project.ProjectMemberAdminAllQueryDTO;
+import sansam.team.project.query.service.ProjectMemberQueryService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/admin/project/member")
@@ -11,6 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Project Member Admin API", description = "프로젝트 회원 관리자 API")
 public class ProjectMemberAdminQueryCotroller {
 
-    // 프로젝트에 들어간 회원 전체 조회
 
+    private final ProjectMemberQueryService projectMemberQueryService;
+
+    // 프로젝트에 들어간 회원 전체 조회
+    @Operation(summary = "프로젝트 회원 전체 조회", description = "프로젝트 번호로 해당 프로젝트의 회원들을 조회합니다.")
+    @GetMapping("/{projectSeq}")
+    public List<ProjectMemberAdminAllQueryDTO> getProjectMembersByProjectSeq(@PathVariable Long projectSeq) {
+        return projectMemberQueryService.findProjectMembers(projectSeq);
+    }
 }
