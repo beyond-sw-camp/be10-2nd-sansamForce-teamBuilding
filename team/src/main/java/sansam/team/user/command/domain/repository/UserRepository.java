8a2fd747ce0143b0e95eb0a133d0f400589cdd40
person76 <94957124+person76@users.noreply.github.com>
2024-09-29 17:54:59 +0900

    userRepository에 findById 추가

--- team/src/main/java/sansam/team/user/command/domain/repository/UserRepository.java
+++ team/src/main/java/sansam/team/user/command/domain/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package sansam.team.user.command.domain.repository;
 
+import sansam.team.team.command.domain.aggregate.entity.TeamMember;
 import sansam.team.user.command.domain.aggregate.entity.User;
 
 import java.util.Optional;
@@ -7,4 +8,6 @@ import java.util.Optional;
 public interface UserRepository {
 
     User save(User user);
+
+    Optional<User> findById(long userSeq);
 }
