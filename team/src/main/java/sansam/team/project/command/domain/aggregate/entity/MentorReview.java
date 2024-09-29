988189189271b6976e97c62e1260af3fb59d833d
kookong2 <hy010309@naver.com>
2024-09-29 20:24:08 +0900

    [FEAT] MentorReview 엔티티 추가

---
+++ team/src/main/java/sansam/team/project/command/domain/aggregate/entity/MentorReview.java
@@ -0,0 +1,27 @@
+package sansam.team.project.command.domain.aggregate.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sansam.team.common.aggregate.entity.BaseTimeEntity;
+
+@Entity
+@Table(name = "tbl_mentor_review")
+@Getter
+@NoArgsConstructor
+public class MentorReview extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mentorReviewSeq;                           // 프로젝트 강사 평가 번호
+
+    private Long projectUserSeq;                          // 프로젝트 회원 번호
+
+    private Long projectMentorSeq;                          // 프로젝트 강사 번호
+
+    private double mentorReviewStar;                        // 강사 평가 별점
+
+    private String mentorReviewContent;                     // 강사 평가 상세 내용
+
+
+}
