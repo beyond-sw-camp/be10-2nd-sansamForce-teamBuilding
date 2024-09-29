c6a3b40897383aa516af914790ae3750ad907236
kookong2 <hy010309@naver.com>
2024-09-29 22:04:50 +0900

    [FEAT] 멘토 평가 생성 서비스 로직 구현

---
+++ team/src/main/java/sansam/team/project/command/application/service/mentor/MentorReviewService.java
@@ -0,0 +1,38 @@
+package sansam.team.project.command.application.service.mentor;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import sansam.team.common.jwt.SecurityUtil;
+import sansam.team.project.command.application.dto.mentor.MentorReviewCreateDTO;
+import sansam.team.project.command.domain.aggregate.entity.MentorReview;
+import sansam.team.project.command.domain.repository.mentor.MentorReviewRepository;
+import sansam.team.user.command.domain.aggregate.entity.User;
+
+@Service
+@RequiredArgsConstructor
+public class MentorReviewService {
+
+    private final MentorReviewRepository mentorReviewRepository;
+    private final ModelMapper modelMapper;
+
+    /* 멘토 평가 생성 로직 */
+    @Transactional
+    public MentorReview createMentorReview(Long userSeq, MentorReviewCreateDTO mentorReviewCreateDTO){
+
+        User mentor = SecurityUtil.getAuthenticatedUser();
+
+        if(mentor.getUserSeq() == null){
+            throw new IllegalArgumentException("User Seq is null");
+        }
+
+        MentorReview mentorReview = modelMapper.map(mentorReviewCreateDTO, MentorReview.class);
+        mentorReview.projectMentorSeq(mentor.getUserSeq());
+        mentorReview.projectUserSeq(userSeq);
+
+        mentorReviewRepository.save(mentorReview);
+
+        return mentorReview;
+    }
+}
