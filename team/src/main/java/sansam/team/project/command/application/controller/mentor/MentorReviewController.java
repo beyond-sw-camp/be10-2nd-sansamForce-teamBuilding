20a0d53f1ff6a7741433c3c2c3facd4444be3468
kookong2 <hy010309@naver.com>
2024-09-30 12:12:54 +0900

    [REFACTOR] 멘토 평가 CUD 엔드포인트 수정

--- team/src/main/java/sansam/team/project/command/application/controller/mentor/MentorReviewController.java
+++ team/src/main/java/sansam/team/project/command/application/controller/mentor/MentorReviewController.java
@@ -6,12 +6,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import sansam.team.common.response.ApiResponse;
 import sansam.team.common.response.ResponseUtil;
-import sansam.team.project.command.application.dto.mentor.MentorReviewDTO;
+import sansam.team.project.command.application.dto.mentor.MentorReviewCreateDTO;
+import sansam.team.project.command.application.dto.mentor.MentorReviewUpdateDTO;
 import sansam.team.project.command.domain.aggregate.entity.MentorReview;
 import sansam.team.project.command.application.service.mentor.MentorReviewService;
 
 @RestController
-@RequestMapping("/api/v1/project/mentor/review")
+@RequestMapping("/api/v1/project/{projectSeq}/mentor/review")
 @RequiredArgsConstructor
 @Tag(name = "Project Mentor Review API", description = "프로젝트 강사 평가 API")
 public class MentorReviewController {
@@ -19,14 +20,14 @@ public class MentorReviewController {
     private final MentorReviewService mentorReviewService;
 
     /* 강사 평가 추가 로직 */
-    @PostMapping("/{projectMemberSeq}")
+    @PostMapping
     @Operation(summary = "프로젝트 별 강사 평가 추가 API", description = "강사만 평가 추가를 할 수 있음")
     public ApiResponse<?> createMentorReview(
-            @PathVariable Long projectMemberSeq,
-            @RequestBody MentorReviewDTO mentorReviewDTO) {
+            @PathVariable Long projectSeq,
+            @RequestBody MentorReviewCreateDTO mentorReviewCreateDTO) {
         try {
             // MentorReview 생성
-            MentorReview mentorReview = mentorReviewService.createMentorReview(projectMemberSeq, mentorReviewDTO);
+            MentorReview mentorReview = mentorReviewService.createMentorReview(mentorReviewCreateDTO);
 
             // 성공 응답 반환
             return ResponseUtil.successResponse("Mentor review created successfully").getBody();
@@ -40,14 +41,14 @@ public class MentorReviewController {
     }
 
     /* 강사 평가 수정 로직 */
-    @PutMapping("/{projectMemberSeq}")
+    @PutMapping("/{mentorReviewSeq}")
     @Operation(summary = "프로젝트 별 강사 평가 수정 API", description = "강사만 평가 수정을 할 수 있음")
     public ApiResponse<?> updateMentorReview(
-            @PathVariable Long projectMemberSeq,
-            @RequestBody MentorReviewDTO mentorReviewDTO) {
+            @PathVariable Long mentorReviewSeq,
+            @RequestBody MentorReviewUpdateDTO mentorReviewUpdateDTO) {
         try {
             // MentorReview 수정
-            MentorReview updatedMentorReview = mentorReviewService.updateMentorReview(projectMemberSeq, mentorReviewDTO);
+            MentorReview updatedMentorReview = mentorReviewService.updateMentorReview(mentorReviewSeq, mentorReviewUpdateDTO);
 
             // 성공 응답 반환
             return ResponseUtil.successResponse("Mentor review updated successfully").getBody();
@@ -61,12 +62,12 @@ public class MentorReviewController {
     }
 
     /* 멘토 평가 삭제 로직 */
-    @DeleteMapping("/{projectMemberSeq}")
+    @DeleteMapping("/{mentorReviewSeq}")
     @Operation(summary = "프로젝트 별 강사 평가 삭제 API", description = "강사만 평가 삭제를 할 수 있음")
-    public ApiResponse<?> deleteMentorReview(@PathVariable Long projectMemberSeq) {
+    public ApiResponse<?> deleteMentorReview(@PathVariable Long mentorReviewSeq) {
         try {
             // MentorReview 삭제
-            mentorReviewService.deleteMentorReview(projectMemberSeq);
+            mentorReviewService.deleteMentorReview(mentorReviewSeq);
 
             // 성공 응답 반환
             return ResponseUtil.successResponse("Mentor review deleted successfully").getBody();
--- team/src/main/java/sansam/team/project/command/application/service/mentor/MentorReviewService.java
+++ team/src/main/java/sansam/team/project/command/application/service/mentor/MentorReviewService.java
@@ -5,11 +5,13 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import sansam.team.common.jwt.SecurityUtil;
-import sansam.team.project.command.application.dto.mentor.MentorReviewDTO;
+import sansam.team.project.command.application.dto.mentor.MentorReviewCreateDTO;
+import sansam.team.project.command.application.dto.mentor.MentorReviewUpdateDTO;
 import sansam.team.project.command.domain.aggregate.entity.MentorReview;
 import sansam.team.project.command.domain.aggregate.entity.ProjectMember;
 import sansam.team.project.command.domain.repository.mentor.MentorReviewRepository;
 import sansam.team.project.command.domain.repository.project.ProjectMemberRepository;
+import sansam.team.project.command.domain.repository.project.ProjectRepository;
 import sansam.team.user.command.domain.aggregate.entity.User;
 
 @Service
@@ -17,12 +19,13 @@ import sansam.team.user.command.domain.aggregate.entity.User;
 public class MentorReviewService {
 
     private final MentorReviewRepository mentorReviewRepository;
+    private final ProjectRepository projectRepository;
     private final ProjectMemberRepository projectMemberRepository;
     private final ModelMapper modelMapper;
 
     /* 멘토 평가 생성 로직 */
     @Transactional
-    public MentorReview createMentorReview(Long projectMemberSeq, MentorReviewDTO mentorReviewDTO){
+    public MentorReview createMentorReview(MentorReviewCreateDTO mentorReviewCreateDTO){
 
         User mentor = SecurityUtil.getAuthenticatedUser(); // 인증된 멘토 가져오기
 
@@ -31,11 +34,11 @@ public class MentorReviewService {
         }
 
         // ProjectMemberSeq로 프로젝트 회원을 조회
-        ProjectMember projectMember = projectMemberRepository.findById(projectMemberSeq)
+        ProjectMember projectMember = projectMemberRepository.findById(mentorReviewCreateDTO.getProjectMemberSeq())
                 .orElseThrow(() -> new IllegalArgumentException("Invalid ProjectMemberSeq"));
 
         // MentorReview 엔티티 생성 및 값 설정
-        MentorReview mentorReview = modelMapper.map(mentorReviewDTO, MentorReview.class);
+        MentorReview mentorReview = modelMapper.map(mentorReviewCreateDTO, MentorReview.class);
         mentorReview.projectMentorSeq(mentor.getUserSeq()); // 멘토 번호 설정
         mentorReview.projectUserSeq(projectMember.getProjectMemberSeq()); // 프로젝트 회원 번호 설정
 
@@ -47,21 +50,21 @@ public class MentorReviewService {
 
     /* 멘토 평가 수정 로직 */
     @Transactional
-    public MentorReview updateMentorReview(Long projectMemberSeq, MentorReviewDTO mentorReviewDTO){
+    public MentorReview updateMentorReview(Long mentorReviewSeq, MentorReviewUpdateDTO mentorReviewUpdateDTO){
 
-        MentorReview mentorReview = mentorReviewRepository.findByProjectMemberSeq(projectMemberSeq)
+        MentorReview mentorReview = mentorReviewRepository.findById(mentorReviewSeq)
                 .orElseThrow(() -> new IllegalArgumentException("Mentor review not found for the given ProjectMemberSeq"));
 
-        mentorReview.updateMentorReview(mentorReviewDTO);
+        mentorReview.updateMentorReview(mentorReviewUpdateDTO);
 
         return mentorReview;
     }
 
     /* 멘토 평가 삭제 로직 */
     @Transactional
-    public void deleteMentorReview(Long projectMemberSeq) {
+    public void deleteMentorReview(Long mentorReviewSeq) {
         // projectMemberSeq로 멘토 평가 조회
-        MentorReview mentorReview = mentorReviewRepository.findByProjectMemberSeq(projectMemberSeq)
+        MentorReview mentorReview = mentorReviewRepository.findById(mentorReviewSeq)
                 .orElseThrow(() -> new IllegalArgumentException("Mentor review not found for the given ProjectMemberSeq"));
 
         // 멘토 평가 삭제
