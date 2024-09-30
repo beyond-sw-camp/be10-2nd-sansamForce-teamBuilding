db344ba6fbe555d9cb4dbe89307ccd41d241064d
kookong2 <hy010309@naver.com>
2024-10-01 00:31:38 +0900

    [REFACTOR] AdminProjectBoardController 응답 값 수정

--- team/src/main/java/sansam/team/project/command/application/controller/AdminProjectBoardController.java
+++ team/src/main/java/sansam/team/project/command/application/controller/AdminProjectBoardController.java
@@ -5,10 +5,13 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import sansam.team.common.response.ApiResponse;
+import sansam.team.common.response.ResponseUtil;
 import sansam.team.project.command.application.dto.AdminProjectApplyMemberDTO;
 import sansam.team.project.command.application.dto.AdminProjectBoardCreateDTO;
 import sansam.team.project.command.application.dto.AdminProjectBoardUpdateDTO;
 import sansam.team.project.command.application.service.AdminProjectBoardService;
+import sansam.team.project.command.domain.aggregate.entity.MentorReview;
 import sansam.team.project.command.domain.aggregate.entity.ProjectApplyMember;
 import sansam.team.project.command.domain.aggregate.entity.ProjectBoard;
 
@@ -22,41 +25,78 @@ public class AdminProjectBoardController {
 
     @PostMapping
     @Operation(summary = "프로젝트 게시물 추가", description = "프로젝트 게시물 추가 API (관리자만 가능)")
-    public ResponseEntity<ProjectBoard> createProjectBoard(
-            @RequestBody AdminProjectBoardCreateDTO adminProjectBoardCreateDTO) { // UserPrincipal을 주입받음
+    public ApiResponse<?> createProjectBoard(
+            @RequestBody AdminProjectBoardCreateDTO adminProjectBoardCreateDTO) {
 
-        // 서비스로 전달하여 ProjectBoard 생성
-        ProjectBoard projectBoard = adminProjectBoardService.createProjectBoard(adminProjectBoardCreateDTO);
+        try {
+            // 프로젝트 게시물 생성 요청
+            ProjectBoard projectBoard = adminProjectBoardService.createProjectBoard(adminProjectBoardCreateDTO);
 
-        return ResponseEntity.ok(projectBoard);
+            return ResponseUtil.successResponse("Project created successfully").getBody();
+        }catch (IllegalArgumentException e){
+
+            return ResponseUtil.failureResponse(e.getMessage(), "USER_SEQ_NULL").getBody();
+        }catch (Exception e){
+
+            return ResponseUtil.exceptionResponse(e, "PROJECT_CREATE_ERROR").getBody();
+        }
+
     }
 
     @PutMapping("/{projectBoardSeq}")
     @Operation(summary = "프로젝트 게시물 수정", description = "프로젝트 게시물 수정 API (관리자만 가능)")
-    public ResponseEntity<ProjectBoard> updateProjectBoard(
+    public ApiResponse<?> updateProjectBoard(
             @PathVariable Long projectBoardSeq,
             @RequestBody AdminProjectBoardUpdateDTO adminProjectBoardUpdateDTO) {
-        // 프로젝트 게시물 업데이트 요청
-        ProjectBoard updatedProjectBoard = adminProjectBoardService.updateProjectBoard(projectBoardSeq, adminProjectBoardUpdateDTO);
-        return ResponseEntity.ok(updatedProjectBoard);
+
+        try {
+            // 프로젝트 게시물 업데이트 요청
+            ProjectBoard updatedProjectBoard = adminProjectBoardService.updateProjectBoard(projectBoardSeq, adminProjectBoardUpdateDTO);
+
+            return ResponseUtil.successResponse("Project updated successfully").getBody();
+        }catch (IllegalArgumentException e){
+
+            return ResponseUtil.failureResponse(e.getMessage(), "PROJECT_BOARD_SEQ_NULL").getBody();
+        }catch (Exception e){
+
+            return ResponseUtil.exceptionResponse(e, "PROJECT_UPDATE_ERROR").getBody();
+        }
     }
 
     @DeleteMapping("/{projectBoardSeq}")
     @Operation(summary = "프로젝트 게시물 삭제", description = "프로젝트 게시물 삭제 API (관리자만 가능), 테스트 용도 삭제 API")
-    public ResponseEntity<Void> deleteProjectBoard(@PathVariable Long projectBoardSeq) {
-        adminProjectBoardService.deleteProjectBoard(projectBoardSeq);
-        return ResponseEntity.noContent().build();
+    public ApiResponse<?> deleteProjectBoard(@PathVariable Long projectBoardSeq) {
+        try {
+            // 프로젝트 게시물  삭제요청
+            adminProjectBoardService.deleteProjectBoard(projectBoardSeq);
+
+            return ResponseUtil.successResponse("Project deleted successfully").getBody();
+        }catch (Exception e){
+
+            return ResponseUtil.exceptionResponse(e, "PROJECT_DELETE_ERROR").getBody();
+        }
     }
 
     // 신청 회원의 상태 업데이트 API
     @PutMapping("/{projectBoardSeq}/apply-member/{applyMemberSeq}")
     @Operation(summary = "프로젝트 게시물 신청 회원 상태 수정", description = "관리자가 해당 게시물에 신청한 회원의 상태를 조정하는 API")
-    public ResponseEntity<ProjectApplyMember> updateApplyMemberStatus(
+    public ApiResponse<?> updateApplyMemberStatus(
             @PathVariable Long projectBoardSeq,
             @PathVariable Long applyMemberSeq,
             @RequestBody AdminProjectApplyMemberDTO adminProjectApplyMemberDTO) {
-        // 서비스로 상태 업데이트 요청
-        ProjectApplyMember updatedApplyMember = adminProjectBoardService.updateApplyMemberStatus(projectBoardSeq, applyMemberSeq, adminProjectApplyMemberDTO);
-        return ResponseEntity.ok(updatedApplyMember);
+
+        try {
+            // 서비스로 상태 업데이트 요청
+            ProjectApplyMember updatedApplyMember = adminProjectBoardService.updateApplyMemberStatus(projectBoardSeq, applyMemberSeq, adminProjectApplyMemberDTO);
+
+            // 성공 응답 반환
+            return ResponseUtil.successResponse("Project board apply member updated successfully").getBody();
+        } catch (IllegalArgumentException e) {
+            // 실패 응답 반환 (예외 발생 시)
+            return ResponseUtil.failureResponse(e.getMessage(), "APPLY_MEMBER_SEQ_NULL").getBody();
+        } catch (Exception e) {
+            // 그 외 예외 처리
+            return ResponseUtil.exceptionResponse(e, "PROJECT_BOARD_APPLY_MEMBER_ERROR").getBody();
+        }
     }
 }
