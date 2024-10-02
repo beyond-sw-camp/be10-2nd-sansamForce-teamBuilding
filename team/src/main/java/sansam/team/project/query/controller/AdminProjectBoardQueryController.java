d5d5d08490cc63511af4441b32f3d7b70b284dcc
kookong2 <hy010309@naver.com>
2024-10-03 03:11:43 +0900

    [REFACTOR] AdminProjectBoardQueryController ApiResponse 응답 값 사용

--- team/src/main/java/sansam/team/project/query/controller/AdminProjectBoardQueryController.java
+++ team/src/main/java/sansam/team/project/query/controller/AdminProjectBoardQueryController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sansam.team.common.response.ApiResponse;
 import sansam.team.project.query.dto.ProjectApplyMemberQueryDTO;
 import sansam.team.project.query.dto.ProjectBoardAllQueryDTO;
 import sansam.team.project.query.dto.AdminProjectBoardDTO;
@@ -25,22 +26,25 @@ public class AdminProjectBoardQueryController {
     /* 프로젝트 게시물 전체 조회 */
     @GetMapping
     @Operation(summary = "프로젝트 게시물 전체 조회", description = "프로젝트 게시물 전체 조회 API (관리자만 가능)")
-    public List<ProjectBoardAllQueryDTO> getAllProjectBoards() {
-        return projectBoardQueryService.getAllProjectBoards(); // ResponseEntity 생략하고 DTO 반환
+    public ApiResponse<List<ProjectBoardAllQueryDTO>> getAllProjectBoards() {
+        List<ProjectBoardAllQueryDTO> projectBoards = projectBoardQueryService.getAllProjectBoards();
+        return ApiResponse.ofSuccess(projectBoards);
     }
 
     /* 프로젝트 게시물 상세 조회 */
     @GetMapping("/{projectBoardSeq}")
     @Operation(summary = "프로젝트 게시물 상세 조회", description = "프로젝트 게시물 상세 조회 API (관리자만 가능)")
-    public AdminProjectBoardDTO getProjectBoardById(@PathVariable Long projectBoardSeq) {
-        return projectBoardQueryService.getProjectBoardByIdForAdmin(projectBoardSeq); // ResponseEntity 생략하고 DTO 반환
+    public ApiResponse<AdminProjectBoardDTO> getProjectBoardById(@PathVariable Long projectBoardSeq) {
+        AdminProjectBoardDTO projectBoard = projectBoardQueryService.getProjectBoardByIdForAdmin(projectBoardSeq);
+        return ApiResponse.ofSuccess(projectBoard);
     }
 
     /* 프로젝트 신청 회원 리스트 조회 */
     @GetMapping("/{projectBoardSeq}/apply-member")
     @Operation(summary = "프로젝트 게시물 신청 회원 리스트 조회", description = "프로젝트 게시물 신청 회원 리스트 조회 API (관리자만 가능)")
-    public List<ProjectApplyMemberQueryDTO> getApplyMembers(
+    public ApiResponse<List<ProjectApplyMemberQueryDTO>> getApplyMembers(
             @PathVariable Long projectBoardSeq) {
-        return projectBoardQueryService.findApplyMembersByProjectBoardSeq(projectBoardSeq); // ResponseEntity 생략하고 DTO 반환
+        List<ProjectApplyMemberQueryDTO> applyMembers = projectBoardQueryService.findApplyMembersByProjectBoardSeq(projectBoardSeq);
+        return ApiResponse.ofSuccess(applyMembers);
     }
 }
