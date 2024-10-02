e7537a96b54d34b842e2ae9e6cae5b90ad0cfdf8
kookong2 <hy010309@naver.com>
2024-10-03 03:37:02 +0900

    [REFACTOR] TeamQueryController ApiResponse 응답 값으로 변경

--- team/src/main/java/sansam/team/team/query/controller/TeamQueryController.java
+++ team/src/main/java/sansam/team/team/query/controller/TeamQueryController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sansam.team.common.response.ApiResponse;
 import sansam.team.team.query.dto.TeamFindByIdResponse;
 import sansam.team.team.query.dto.TeamResponse;
 import sansam.team.team.query.service.TeamQueryService;
@@ -23,15 +24,15 @@ public class TeamQueryController {
 
     @GetMapping
     @Operation(summary = "팀 리스트 조회")
-    public TeamResponse selectTeamList() {
-        return teamQueryService.selectTeamList();
-
+    public ApiResponse<TeamResponse> selectTeamList() {
+        TeamResponse teamList = teamQueryService.selectTeamList();
+        return ApiResponse.ofSuccess("Team list retrieved successfully", teamList);
     }
 
     @GetMapping("/{teamSeq}")
     @Operation(summary = "팀 조회")
-    public TeamFindByIdResponse selectTeamById(@PathVariable Long teamSeq) {
-        return teamQueryService.selectTeamById(teamSeq);
-
+    public ApiResponse<TeamFindByIdResponse> selectTeamById(@PathVariable Long teamSeq) {
+        TeamFindByIdResponse team = teamQueryService.selectTeamById(teamSeq);
+        return ApiResponse.ofSuccess("Team retrieved successfully", team);
     }
 }
