5e374f1d92007106752e1467b10980b947a19054
beanteacher <dh65432@naver.com>
2024-10-03 12:00:06 +0900

    [REFACTOR] MSA Setting

---
+++ user/src/main/java/com/sansam/user/query/controller/UserQueryController.java
@@ -0,0 +1,20 @@
+package com.sansam.user.query.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/user")
+public class UserQueryController {
+
+    private final Environment env;
+
+    @GetMapping("/health")
+    public String health() {
+        return "I'm Working in User Service " + env.getProperty("local.server.port");
+    }
+}
